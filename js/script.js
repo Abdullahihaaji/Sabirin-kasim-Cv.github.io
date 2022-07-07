2008ff28ad8e94b4941c8316e414336cee625c24
Abdalla Omar Hassan <74817363+Abdullahihaaji@users.noreply.github.com>
2022-07-07 00:23:00 -0700

    sabnaa cv

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/* ============================== typing animation ============================ */
+var typed = new Typed(".typing",{
+    strings:["","Photographer Woman","Arts Woman","","YouTuber","Bussiness Woman"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
